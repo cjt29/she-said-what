381af5f563acf23c1435ad86669760c1802bbd73
CJT <cjt2908@gmail.com>
2024-08-31 15:26:20 -0400

    Adds auto-imports & component types

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import laravel from 'laravel-vite-plugin'
+import autoimport from 'unplugin-auto-import/vite'
+import components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   plugins: [
+    autoimport({
+      vueTemplate: true,
+      dts: 'resources/js/scripts/types/auto-imports.d.ts',
+      imports: [
+        'vue',
+        { '@inertiajs/vue3': ['router', 'useForm', 'usePage'] },
+      ],
+      dirs: ['resources/js/scripts/composables'],
+    }),
+    components({
+      dts: 'resources/js/scripts/types/components.d.ts',
+      resolvers: [
+        (name: string) => {
+          const components = ['Link', 'Head']
+
+          if (components.includes(name)) {
+            return {
+              name,
+              from: '@inertiajs/vue3',
+            }
+          }
+        },
+      ],
+      dirs: ['resources/js/components/global'],
+    }),
     laravel({
       input: 'resources/js/app.ts',
       refresh: true,
